b1521043763e65964202eeaad1199fb0d740a0d8
Taras <topanasenko85@gmail.com>
2023-05-23 10:28:56 +0300

    start

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename="/goit-react-hw-05-movies">
     <App />
   </React.StrictMode>
 );
