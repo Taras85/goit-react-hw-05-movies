761fa8922f40423aa79663844f4e6ab20c183f11
Taras <topanasenko85@gmail.com>
2023-05-23 12:26:17 +0300

    folder structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode basename="/goit-react-hw-05-movies">
-    <App />
+  <React.StrictMode >
+      <BrowserRouter basename="/goit-react-hw-05-movies">
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
+  
 );
---
+++ src/pages/GetTrending.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GetTrending = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GetTrending
