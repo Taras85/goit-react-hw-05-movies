39cef87cb4c3c11cdffb049292c569bcdfec501f
Taras <topanasenko85@gmail.com>
2023-05-26 20:45:02 +0300

    lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,26 @@
-import GetTrending from "pages/GetTrending";
-import MovieDetails from "pages/MovieDetails";
-import Movies from "pages/Movies";
+
 import { Navigate, Route, Routes } from "react-router-dom";
+import { lazy } from 'react';
 import Header from "./Header/Header";
-import Cast from "./Cast/Cast";
-import Review from "./Reviews/Review";
-// import s from './App.module.css'
+// import GetTrending from "pages/GetTrending";
+// import MovieDetails from "pages/MovieDetails";
+// import Movies from "pages/Movies";
+// import Cast from "./Cast/Cast";
+// import Review from "./Reviews/Review";
+
+
+const GetTrending = lazy(() => import('../pages/GetTrending'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Review = lazy(() => import('./Reviews/Review'));
+
+
 
 export const App = () => {
   return (
-    <div
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   color: '#010101'
-      // }}
-    >
+    <div >
       <Routes>
-
           <Route path="/" element={<Header/>}>
             <Route index element={<GetTrending />}/>
             <Route path="movies" element={<Movies />}/>
